2263ec10985703437550f2c2cc5c5a1ff28875a0
Luke-Sheltraw <lukesheltraw@gmail.com>
2024-03-03 18:23:02 -0800

    confirmation screen formatting

--- app/fragments/ConfirmationFragment/fragment.tsx
+++ app/fragments/ConfirmationFragment/fragment.tsx
@@ -8,7 +8,10 @@ const ConfirmationFragment = () => {
     </p>
     <p className={ styles.paragraph }>
       <strong className={ styles.warning }>Note: </strong>
-      The posts shown in this study were constructed purely for the sake of research. You should <strong className={ styles.warning }>not</strong> take any information shown to be truthful. Namely, Rachel Bernays and Lachlan Morgan are wholly fabricated characters, and neither them nor any story involving them contain any reflection of or basis in actual events.
+      The posts shown in this study were constructed purely for the sake of research. You should <strong className={ styles.warning }>not</strong> take any information shown to be truthful.
+    </p>
+    <p className={ styles.paragraph }>
+      Namely, Rachel Bernays and Lachlan Morgan are <strong className={ styles.warning }>wholly fabricated characters</strong>, and neither them nor any story involving them contain any reflection of or basis in actual events.
     </p>
     </>
   );
