9fa61a397d0071e5d118c3cdd9f91a60e795f72c
Luke-Sheltraw <lukesheltraw@gmail.com>
2024-03-03 18:24:51 -0800

    build fixes

--- app/helpers.ts
+++ app/helpers.ts
@@ -1,8 +1,8 @@
 /* Modified from http://detectmobilebrowser.com */
 export function checkIfUserAgentIsMobile() {
-  if (!navigator) return false;
+  const userAgent: string = navigator?.userAgent || navigator?.vendor;
+  if (!userAgent) return false;
 
-  const userAgent: string = navigator.userAgent || navigator.vendor;
   return (
     /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
       userAgent
